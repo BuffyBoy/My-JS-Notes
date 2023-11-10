8be8437510ecb582379e0ed68aca342ec8e09a37
Farman Haris <farmanharis66@gmail.com>
2023-11-10 21:13:08 +0500

    Datatypes

---
+++ 02_Datatypes.js
@@ -0,0 +1,18 @@
+// "use strick"; //treat all JS code as newer version
+
+// alert("hello") // we are using node JS, not browser
+
+let name = "Farman Haris"
+let age = 18
+let isLoggedIn = false
+let state
+
+//number => 2 to power of 53
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined => undefined
+//symbol => unique
+
+console.log(typeof null); //null is an Object in JS
+console.log(undefined); //undefined is a datatype in itself JS
