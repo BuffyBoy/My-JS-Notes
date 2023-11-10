7dec7f6a81d885b9a06e60bc9235697a4d410152
Farman Haris <farmanharis66@gmail.com>
2023-11-10 20:50:41 +0500

    Variables

---
+++ 01_Variables.js
@@ -0,0 +1,18 @@
+const accountId = 14547;
+let accountEmail = "high4tech360@gmail.com";
+var accountPassword = "fklsadhkjfsa";
+accountCity = "Lahore";
+
+// accountId = 2; not allowed
+
+accountEmail = "hj@dsh.com"
+accountPassword = "4654132"
+accountCity = "Islamabad"
+
+/*
+Preferd not to use var
+because of issue in block scope and functional scope
+*/
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
