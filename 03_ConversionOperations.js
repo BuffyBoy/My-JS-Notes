cbec950caa47ac611dba8a34d9cdbd8e2517f269
Farman Haris <farmanharis66@gmail.com>
2023-11-11 15:09:06 +0500

    Conversion of Datatypes

---
+++ 03_ConversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33sda"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN (not a number)
+//true => 1 ; false => 0
+
+let isLoggesIn = 1
+
+let booleanIsLoggesIn = Boolean(isLoggesIn)
+console.log(booleanIsLoggesIn);
+
+//true => 1 ; false => 0
+//"" => false
+//"Farman" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
