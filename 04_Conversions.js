f9350e26cb6c3b70ade3b27939f048dd5705fcf8
Farman Haris <farmanharis66@gmail.com>
2023-11-11 18:33:03 +0500

    Conversions in Datatypes

---
+++ 04_Conversions.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1 );
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === Strickt check (check datatype also with values)
+
+console.log("2" === 2)
