4439a19285e0e6a8ca8c65aa6fbb32c7fbf5353d
Farman Haris <farmanharis66@gmail.com>
2023-11-18 01:54:00 +0500

    Strings in JS

---
+++ Strings.js
@@ -0,0 +1,14 @@
+const name = "Farman"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value")
+
+// A modern way to execute Strings
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const GameName = new String('farman')
+
+console.log(GameName[0]);
+console.log(GameName[3]);
+console.log(GameName.__proto__);
