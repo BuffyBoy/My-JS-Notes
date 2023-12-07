fcce7d5d8fcc1ae4ef89686ca33e22fc11bcbeba
Farman Haris <farmanharis66@gmail.com>
2023-12-07 18:23:44 +0500

    Added Practice Set 1

--- 01_Variables.js
+++ 01_Variables.js
@@ -16,3 +16,20 @@ because of issue in block scope and functional scope
 
 // console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+//Practice Set 1:-
+const Product = {
+    name: "Ball Pen",
+    price: 45,
+    rating: 3.5,
+    offer: "20% discount"
+}
+console.log(Product);
+
+const profile = {
+    username: "@farman.haris",
+    isFollow: true,
+    following: 123,
+    followers: 123,
+}
+console.log(profile);
