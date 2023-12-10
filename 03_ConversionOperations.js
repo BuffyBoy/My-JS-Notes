c8c5c2202c02d1e91d2e4640a7fe9ce91ebb7329
Farman Haris <farmanharis66@gmail.com>
2023-12-10 23:31:22 +0500

    Operators & Conditionals

--- 03_ConversionOperations.js
+++ 03_ConversionOperations.js
@@ -37,10 +37,10 @@ console.log(negValue);*/
 // console.log(2/2); divid
 // console.log(2%2); reaminder/modulus
 
-let str1 = "hello"
-let str2 = "Farman"
+// let str1 = "hello"
+// let str2 = "Farman"
 
-let str3 = str1 + str2
+// let str3 = str1 + str2
 /*console.log(str3);
 
 console.log("1" + 2);
@@ -52,9 +52,49 @@ console.log("1" + [2 + 2]);*/
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, num3
- num1 = num2 = num3 = 2+2
+// let num1, num2, num3
+//  num1 = num2 = num3 = 2+2
 
- let gameCounter = 100
- ++gameCounter;
- console.log(gameCounter);
+//  let gameCounter = 100
+//  ++gameCounter;
+//  console.log(gameCounter);
+
+
+ //Practice set Operators & Conditionals
+ //Problem 01
+
+ /*let value = 36;
+
+ if (value%5 === 0) {
+    console.log("Multiple of 5");
+ }
+ else{
+    console.log("Not a Multiple of 5");
+ }*/
+
+ //Problem 02
+
+ let num = 79;
+ if (num <= 49) {
+    console.log("Grade F");  
+ }
+
+ else if (num <= 59) {
+    console.log("Grade D");  
+ }
+
+ else if (num <= 69) {
+    console.log("Grade C");  
+ }
+
+ else if (num <= 89) {
+    console.log("Grade B");  
+ }
+
+ else if (num <= 100) {
+    console.log("Grade A");  
+ }
+ 
+ else{
+    console.log("Kina perna e Murray!");
+ }
